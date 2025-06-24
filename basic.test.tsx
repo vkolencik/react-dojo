3c60d3b6ffbbb8973e848338a4defdd9c0a5ac00
Vojtech Kolencik <kolencik@gmail.com>
2025-06-24 09:13:29 +0200

    warmup

--- basic.test.tsx
+++ basic.test.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-import { it } from "vitest";
-import { render } from '@testing-library/react';
+import { expect, it } from "vitest";
+import { act, render, waitFor } from '@testing-library/react';
+import App from './src/App';
 import userEvent from "@testing-library/user-event";
 
-it('renders', () => {
-  const screen = render(<div><button>press</button></div>);
-  const btn = screen.getByRole('button', {name: /press/});
-  userEvent.click(btn);  
+it('renders', async () => {
+  const screen = await act(() => render(<App/>));
+  expect(screen.getByTestId('counter').textContent).toBe('0');
+  const btn = screen.getByRole('button', {name: /press/i});
+  await userEvent.click(btn);
+  expect(screen.getByTestId('counter').textContent).toBe('1')  
 });
